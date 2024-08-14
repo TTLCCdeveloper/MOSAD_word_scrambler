b65275a5f0cbed14a8c9646f8f2f458b0591264f
TTLCCdeveloper <ttlccdevelopers@hit.ac.zw>
2024-08-14 07:25:48 +0200

    Add files via upload

---
+++ js/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "creativity",
+        hint: "Generating new ideas and solutions"
+    },
+    {
+        word: "cybersecurity",
+        hint: "Protecting digital assets and information"
+    },
+    {
+        word: "emotional intelligence",
+        hint: "Understanding and managing emotions,building relationships"
+    },
+    {
+        word: "programming",
+        hint: "Developing software applications and systems."
+    },
+    {
+        word: "biomedical",
+        hint: "Engineering based on biology"
+    },
+    {
+        word: "digital literacy",
+        hint: "Effectively using technology for communication, learning, and productivity"
+    },
+    {
+        word: "innovation",
+        hint: "Developing new solutions for problems"
+    },
+    {
+        word: "google",
+        hint: "Most visited website"
+    },
+    {
+        word: "transplant",
+        hint: "Implanting an organ from a donor to the patient"
+    },
+    {
+        word: "reskilling",
+        hint: "Skilling, ______ and upskillin for socio-economic transformation"
+    },
+    {
+        word: "uranium",
+        hint: "Used in nuclear weaponry"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "artificial intelligence",
+        hint: "Man-made intelligence developed by software engineers"
+    },
+    {
+        word: "nitrogen",
+        hint: "Used in fast freezing of food in the culinary industry"
+    },
+    {
+        word: "sustainability",
+        hint: "Understanding environmental impact and promoting sustainable practices"
+    },
+    {
+        word: "metaverse",
+        hint: "Digital space that merges virtual and physical realities"
+    },
+    {
+        word: "cantilever",
+        hint: "Beam that is supported perpendicularly in construction"
+    },
+    {
+        word: "data analytics",
+        hint: "Making sense of large volumes of data"
+    },
+    {
+        word: "hologram",
+        hint: "Life-like projection of visuals"
+    },
+    {
+        word: "chatGPT",
+        hint: "AI language model"
+    },
+    {
+        word: "Revair",
+        hint: "Hair dryer that dries like a vacuum rather than blowing"
+    },
+    {
+        word: "cryptocurrency",
+        hint: "Online form of money"
+    },
+    {
+        word: "Dall E",
+        hint: "AI image generation model"
+    },
+]
